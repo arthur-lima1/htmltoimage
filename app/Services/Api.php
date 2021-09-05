ebbdcb27b504f0e876e8d67653b6cb942d4b0f58
arthur-lima1 <arthur.lima@inforce.com.br>
2021-09-05 11:29:35 -0300

    basic exception

--- app/Services/Api.php
+++ app/Services/Api.php
@@ -4,45 +4,56 @@ namespace App\Services;
 
 use \GuzzleHttp\Client;
 
-class Api{
+class Api
+{
 
+    /**
+     * Chaves de acesso
+     */
     const USER_ID = 'f908fdde-2e58-43ec-8c11-1bd3a786bb78';
-    const API_KEY= '5c41c08e-acdf-4b58-8a65-744b6db2271b';
+    const API_KEY = '5c41c08e-acdf-4b58-8a65-744b6db2271b';
 
-    public static function generateImage($url, $selector ='') {
-        $data = array('url' => $url);
-       /* $client = new Client();
-        $res = $client->request('POST', 'https://hcti.io/v1/image',[
-            'auth'=>[self::USER_ID, self::API_KEY],
-            'form_params'=> ['url'=>$url]
-        ]);
-        return $res;*/
+    public static function generateImage($url, $selector = '')
+    {
 
+        /**
+         * Dados que serão enviados para a API
+         */
+
+        if (!empty($selector) && strlen($selector) > 0) {
+            $data = array('url' => $url, 'selector' => $selector);
+        } else {
+            $data = array('url' => $url);
+        }
+
+
+        /**
+         * Chamada para a API
+         */
         $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "https://hcti.io/v1/image");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, "https://hcti.io/v1/image");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_USERPWD, self::USER_ID . ":" . self::API_KEY);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_USERPWD, self::USER_ID . ":" . self::API_KEY);
 
-$headers = array();
-$headers[] = "Content-Type: application/x-www-form-urlencoded";
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $headers = array();
+        $headers[] = "Content-Type: application/x-www-form-urlencoded";
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-  echo 'Error:' . curl_error($ch);
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+        $res = json_decode($result, true);
+
+        return array(
+            'status' => 'success',
+            'data' => $res['url']
+        );
+    }
 }
-curl_close ($ch);
-$res = json_decode($result,true);
-return $res['url'];
-
-}
-
-
-
-}
-
--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <?php
 
 require 'vendor/autoload.php';
+
 use \App\Services\Api;
 
 header('Content-Type: application/json');
 
-if(isset($_GET['url']) && !empty($_GET['url'])){
+if (isset($_GET['url']) && !empty($_GET['url'])) {
   $url = urldecode($_GET['url']);
 
-  $imgUrl = Api::generateImage($url, $imgUrl);
+  if (isset($_GET['selector']) && !empty($_GET['selector'])) {
+    $selector = $_GET['selector'];
+  }
 
-  echo json_encode(array('status'=>'Passei da função', 'url'=>$url));
 
-  echo json_encode($imgUrl);
+  try {
 
+    $imgUrl = Api::generateImage($url, $selector);
+
+    if ($imgUrl['data'] === null) {
+      throw new Exception('invalid params');
+    }
+    echo json_encode($imgUrl);
+  } catch (Exception $e) {
+    echo json_encode(array('error' => $e->getMessage()));
+  }
+} else {
+  echo json_encode(array(
+    'status' => 'error',
+    'data' => 'Envie a URL que será renderizada'
+  ));
 }
